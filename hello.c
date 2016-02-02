eac49756bf8b20d2acabe72093ecc2abeb3a39d7
SRMW <srmwconsultants@gmail.com>
2016-02-02 14:57:37 -0800

    First commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello!\n");
+	printf("I am a superhero.\n");
+	printf("I love to code!\n");
+	printf("My father-in-law's birthday is February 29th.\n");
+	return 0;
+}
